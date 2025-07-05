04a999235b555e2c6b8f61a09a43ab78c3238521
kjhgfdsa1014 <kjhgfdsa1014@gmail.com>
2025-07-05 06:09:10 +0000

    Revert "first"
    This reverts commit dcc56a8e655a15c21fa10aaa3d37ba00bb643bda.

--- user/src/pdfWorker.js
+++
@@ -1,7 +0,0 @@
-// src/pdfWorker.js
-import * as pdfjs from "pdfjs-dist/legacy/build/pdf"; // 'legacy' ensures full API
-import worker from "pdfjs-dist/legacy/build/pdf.worker?worker";
-
-pdfjs.GlobalWorkerOptions.workerPort = new worker();
-
-export default pdfjs;
